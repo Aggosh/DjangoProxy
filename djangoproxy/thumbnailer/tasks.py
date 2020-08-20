e851fcc40a966bf1af900a83688600a45a50d95f
Aggosh <bkmzilia@gmail.com>
2020-08-20 13:45:21 +0300

    first

---
+++ djangoproxy/thumbnailer/tasks.py
@@ -0,0 +1,44 @@
+from celery import shared_task
+from celery.schedules import crontab
+from celery import Celery
+
+
+app = Celery()
+
+@shared_task
+def add(x, y):
+    return x + y
+
+
+@shared_task
+def mul(x, y):
+    return x * y
+
+
+@shared_task
+def xsum(numbers):
+    return sum(numbers)
+
+
+@shared_task
+def adding_task(x, y):
+    return x + y
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 10 seconds.
+    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+
+    # Calls test('world') every 30 seconds
+    sender.add_periodic_task(30.0, test.s('world'), expires=10)
+
+    # Executes every Monday morning at 7:30 a.m.
+    sender.add_periodic_task(
+        crontab(hour=7, minute=30, day_of_week=1),
+        test.s('Happy Mondays!'),
+    )
+
+
+@app.task
+def test(arg):
+    print(arg)
